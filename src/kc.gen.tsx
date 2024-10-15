a9d310c6539612e6ad158092e01a9b6bcf8e9a3e
Patrick <patrsc@nubu.at>
2024-10-15 22:22:41 +0200

    Run npm run build-keycloak-theme

--- src/kc.gen.tsx
+++ src/kc.gen.tsx
@@ -10,9 +10,9 @@
 
 import { lazy, Suspense, type ReactNode } from "react";
 
-export type ThemeName = "keycloakify-starter";
+export type ThemeName = "keycloakify-matpackages";
 
-export const themeNames: ThemeName[] = ["keycloakify-starter"];
+export const themeNames: ThemeName[] = ["keycloakify-matpackages"];
 
 export type KcEnvName = never;
 
